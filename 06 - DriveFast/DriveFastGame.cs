0c8449ca4dca40b5b4d2fd8d928c332df3665ce9
Sergy93 <takanuva7493@hotmail.com>
2015-05-12 12:10:46 +0200

    DriveFast WIP

---
+++ 06 - DriveFast/DriveFastGame.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _06___DriveFast
+{
+    public class DriveFastGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        RoadManager roadMng = new RoadManager();
+        private Car Car;
+        private Vector2 Speed = new Vector2(0, 1);
+
+        public DriveFastGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            roadMng.LoadContent(Content);
+
+            graphics.PreferredBackBufferHeight = roadMng.SpriteTexture.Height;
+            graphics.PreferredBackBufferWidth = roadMng.SpriteTexture.Width;
+            graphics.ApplyChanges();
+
+
+            Car = new Car(new Vector2(0, 0), Speed, new Vector2(0, -1));
+
+            var posX = graphics.PreferredBackBufferWidth / 2 - Car.Size.Width * 2;
+            var posY = graphics.PreferredBackBufferHeight - Car.Size.Height;
+
+            Car.Position = new Vector2(posX, posY - 100);
+
+            Car.LoadContent(Content);
+
+        }
+
+
+        protected override void UnloadContent()
+        {
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            roadMng.Update(gameTime, 1);
+            Car.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+
+            roadMng.Draw(spriteBatch);
+            Car.Draw(spriteBatch);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ 06 - DriveFast/RoadManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameHelpers.Classes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _06___DriveFast
+{
+    public class RoadManager : MovingSprite
+    {
+
+        //Float value is the y position
+        private const string Asset = "Road";
+
+        public RoadManager()
+            : base(Asset, Vector2.Zero, new Vector2(0, 1), new Vector2(0, 1))
+        {
+        }
+
+        public override void LoadContent(ContentManager contManager)
+        {
+            base.LoadContent(contManager);
+        }
+
+        public void Update(GameTime gameTime, float speed)
+        {
+            //if (Roads.All(r => r.Value != 0.0f))
+            //{
+            //    Roads.Add(originPosition, 0);
+            //}
+
+            //var temp = Roads;
+            //var keys = new List<Rectangle>(temp.Keys);
+            //foreach (var key in keys)
+            //{
+            //    temp[key] += speed;
+            //}
+            //Roads = temp;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+    }
+}
